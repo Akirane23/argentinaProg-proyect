eadb4cc754586653bdb2f73e9d7ee6405072717f
Akirane23 <evu_23maria@hotmail.com>
2023-12-04 17:52:41 -0300

    Actualizacion

---
+++ integrador-proyecto/src/components/TareasAdd.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TareasAdd = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TareasAdd
---
+++ integrador-proyecto/src/components/Titulo.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+const Titulo = () => {
+  return (
+    <div>
+        <h1>Tareas y proyectos.</h1>
+        <h2>Agrega tus tareas, eventos y proyectos, para tener más organizado tus horarios.</h2>
+    </div>
+  )
+}
+
+export default Titulo
